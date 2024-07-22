f3075fea752905dbd6afeff1143a375c062d23ee
Admin <phama5024@gmail.com>
2024-07-22 11:08:53 +0700

    fix

--- FE/project/src/index.js
+++ FE/project/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <h1>Hello</h1>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <BrowserRouter>
+      <RouterCustom/>
+    </BrowserRouter>
+  );
